0487ed0139205cc599b1807600f8a8c62fbb021f
bradtipler <btipler@gmail.com>
2020-02-11 17:50:08 -0800

    Got the 6th challenge working

--- challenges/01-arrow-functions/06-noParam.js
+++ challenges/01-arrow-functions/06-noParam.js
@@ -3,11 +3,14 @@
 // you should use the empty () syntax of _ syntax.
 
 
-function noParam() {
-
+//function noParam() {
+//
+//}
+let noParam = () => {
+    const firstString = "FirstString";
+    const secondString = "SecondString";
+    return ('${firstString} + ${secondString}');
 }
-
-
 
 
 
